ce9241cf715f147572fd4e7443affe8b2a4312c5
Md. Safirur Rashid <40953267+5AF1@users.noreply.github.com>
2024-12-09 20:48:28 +0600

    further debugging

--- evaluate.py
+++ evaluate.py
@@ -158,6 +158,7 @@ def validate_seismic(model, args, iters=24):
 
         for val_id in tqdm(list(range(len(val_dataset))), desc = f'Val org_pp for {len(val_dataset)} pair'):
             image1, image2, flow_gt, valid_gt = val_dataset[val_id]
+            print(torch.count_nonzero(valid_gt))
             image1 = image1[None].cuda()
             image2 = image2[None].cuda()
 
